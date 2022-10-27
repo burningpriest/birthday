d9856add5e08fa77da6723164f167d8a23ba749b
himanshu <himanshu6295@gmail.com>
2022-10-27 14:39:48 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { Birthday } from './birthday/birthday';
 import React, { useEffect } from 'react';
 
 function App() {
-  const endDate = new Date('Oct 20, 2022, 2:05');
+  const endDate = new Date('Nov 11, 2022, 00:00');
   const currentDate = new Date();
   const diff = endDate.getTime() - currentDate.getTime();
   const ds = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  console.log(ds, hours, minutes, seconds);
   const [[days, hrs, mins, secs], setTime] = React.useState([
     ds,
     hours,
@@ -20,7 +19,7 @@ function App() {
   ]);
   const tick = () => {
     if (hrs == 0 && mins == 0 && secs == 0) {
-      setTime([days - 1, 11, 59, 59]);
+      setTime([days - 1, 23, 59, 59]);
     } else if (mins == 0 && secs == 0) {
       setTime([days, hrs - 1, 59, 59]);
     } else if (secs == 0) {
@@ -28,6 +27,7 @@ function App() {
     } else {
       setTime([days, hrs, mins, secs - 1]);
     }
+    console.log(days, hrs, mins, secs);
   };
   let timeId = 0;
   useEffect(() => {
@@ -46,11 +46,14 @@ function App() {
           <div className='cover'>
             <div className='timeBox'>
               {days.toString().padStart(2, 0)}
-              <div className='small'>Hours</div>
+              <div className='small'>Days</div>
             </div>
-            <div className='timeBox'>{hrs.toString().padStart(2, 0)}</div>
-            <div className='timeBox'>{mins.toString().padStart(2, 0)}</div>
-            <div className='timeBox'>{secs.toString().padStart(2, 0)}</div>
+            <div className='timeBox'>{hrs.toString().padStart(2, 0)}
+            <div className='small'>Hours</div></div>
+            <div className='timeBox'>{mins.toString().padStart(2, 0)}
+            <div className='small'>Minutes</div></div>
+            <div className='timeBox'>{secs.toString().padStart(2, 0)}
+            <div className='small'>Seconds</div></div>
           </div>
         </>
       )}
--- src/birthday/birthday.js
+++ src/birthday/birthday.js
@@ -1,24 +1,29 @@
-import './birthday.css'
-import himaniImg from './himani.jpg'
+import './birthday.css';
+import himaniImg from './himani.jpg';
+import signImg from './signTransparent.png';
 export function Birthday() {
   return (
     <div className='card'>
       <div className='imgBox'>
         <div className='bark'></div>
-        <img src={himaniImg}/>
+        <img src={himaniImg} />
       </div>
       <div className='details'>
-        <h4 className='color1'>You're not that old! (YET)</h4>
-        <h4 className='color2'>(HAPPY BIRTHDAY)</h4>
         <p>Dear Himani,</p>
         <p>I wish we could spend this together</p>
-        <p>Nonetheless, come back soon</p>   
+        <p>Nonetheless, its just one birthday</p>
+        <p>abhi bahut sare sath manane hein</p>
+        <p>ek bar wapis aja, </p>
         <p>I wanna hug you so badly</p>
         <p>Watch game of thrones together</p>
         <p>Cook you tasty food</p>
-        <p>For now party hard:)</p>
+        <p>aur tere hath ke paranthe, muaahh...</p>
+        <p>For now party hard :)</p>
+        <p>jaldi milte hein</p>
         <p class='text-right'>Happy Birthday, Bacche!</p>
-        <p class='text-right'>❤Himanshu</p>
+        <p class='text-right bigger'>
+          ❤<img src={signImg} width={80} height={20} />
+        </p>
       </div>
     </div>
   );
